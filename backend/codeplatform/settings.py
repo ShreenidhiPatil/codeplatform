8fce317ed8237dd322484ed77ecbddf6738e7fc8
Shrinidhi <shrinidhi.patil@kletech.ac.in>
2026-07-10 10:35:25 +0530

    Configure PostgreSQL for Render

--- backend/codeplatform/settings.py
+++ backend/codeplatform/settings.py
@@ -95,20 +95,18 @@ WSGI_APPLICATION = "codeplatform.wsgi.application"
 # DATABASE
 # --------------------------------------------------------------------------
 
+
+
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.mysql",
+        "ENGINE": "django.db.backends.postgresql",
         "NAME": config("DB_NAME"),
         "USER": config("DB_USER"),
         "PASSWORD": config("DB_PASSWORD"),
         "HOST": config("DB_HOST"),
         "PORT": config("DB_PORT"),
-        "OPTIONS": {
-            "charset": "utf8mb4",
-        },
     }
 }
-
 # --------------------------------------------------------------------------
 # PASSWORD VALIDATION
 # --------------------------------------------------------------------------
