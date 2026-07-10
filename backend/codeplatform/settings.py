9f3f169ab3f6a06d23dc8c03b998548f14a6d9e3
Shrinidhi <shrinidhi.patil@kletech.ac.in>
2026-07-10 13:03:05 +0530

    Configure CORS for production

--- backend/codeplatform/settings.py
+++ backend/codeplatform/settings.py
@@ -97,14 +97,9 @@ WSGI_APPLICATION = "codeplatform.wsgi.application"
 # --------------------------------------------------------------------------
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": config("DB_NAME"),
-        "USER": config("DB_USER"),
-        "PASSWORD": config("DB_PASSWORD"),
-        "HOST": config("DB_HOST"),
-        "PORT": config("DB_PORT"),
-    }
+    "default": dj_database_url.config(
+        default=config("DATABASE_URL")
+    )
 }
 
 # --------------------------------------------------------------------------
